5f6151823ef420b60cfa20cd61b727b257bb955a
belalgamal46 <belalgamal00@gmail.com>
2022-11-01 19:43:33 +0200

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './css/styles.scss';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
