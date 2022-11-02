c24f230042e9a0b46a72b203c29563baa6431376
belalgamal46 <belalgamal00@gmail.com>
2022-11-02 15:59:14 +0200

    Add html and styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import _ from 'lodash';
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
 import './css/styles.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
 
-function component() {
-  const element = document.createElement('div');
+const tasks = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'watch a movie',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'eat dinner',
+    completed: false,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const listItems = document.querySelector('.list-items');
 
-  return element;
-}
-
-document.body.appendChild(component());
+tasks.forEach((task) => {
+  listItems.innerHTML += `
+    <li>
+      <input
+        type="checkbox"
+        name="${task.index}"
+        id="${task.index}"
+        title="Check"
+      />
+      <label for="${task.index}" contenteditable="true"> ${task.description} </label>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    </li>
+  `;
+});
