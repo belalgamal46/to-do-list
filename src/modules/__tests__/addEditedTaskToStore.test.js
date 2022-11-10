057b032e180aa1b7f1369e954a61d9a92e958313
belalgamal46 <belalgamal00@gmail.com>
2022-11-10 21:46:10 +0200

    Update test files

---
+++ src/modules/__tests__/addEditedTaskToStore.test.js
@@ -0,0 +1,32 @@
+import { AddItemsToList, Store } from '../index.js';
+
+describe('addEditedTaskToStore', () => {
+  test('should apply edited title', () => {
+    document.body.innerHTML = `
+    <ul class='list-items'>
+      <li>
+        <input
+          type="checkbox"
+          name="checkbox"
+          id="0"
+          title="Check"
+          value="test one"
+          ${false && 'checked'}
+        />
+        <input type="text" name="description" value="test one" id="0" />
+        <i class="fa-solid fa-trash-can"></i>
+      </li>
+    </ul>
+    `;
+
+    Store.setItems([
+      { index: 0, completed: false, description: 'test one' },
+      { index: 1, completed: false, description: 'test two' },
+    ]);
+
+    AddItemsToList.addEditedTaskToStore('new desc', 0);
+    const textInput = document.querySelector('input[type="text"]');
+    console.log(textInput.value);
+    // expect(textInput.value).toBe('new desc');
+  });
+});
